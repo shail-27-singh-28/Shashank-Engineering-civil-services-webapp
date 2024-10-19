a703b7ae9b97a49807467b96419e7bf36b531011
shail-27-singh-28 <shail2728singh@gmail.com>
2024-10-19 15:53:36 +0530

    fade animation add

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -7,27 +7,31 @@ const About = () => {
       <section className="flex justify-center items-center dark:bg-gray-900 bg-white h-screen py-10">
         <div className="border-2 border-blue-500 flex flex-col md:flex-row bg-white rounded-lg shadow-lg max-w-6xl w-full overflow-hidden">
           <div className="flex-1 p-4 dark:text-white dark:bg-gray-500">
-            <h1 className="text-center text-5xl mb-5">About Us</h1>
-            <p className="text-center text-lg leading-relaxed mb-5">
+            <h1 data-aos="fade-up" className="text-center text-5xl mb-5">About Us</h1>
+            <p data-aos="fade-up" data-aos-delay="300" className="text-center text-lg leading-relaxed mb-5">
               Shashank Enginee Civil Services is a leading turnkey provider in
               structural strengthening repairs and retrofitting of all types of
               civil engineering structures. Our key services include:
             </p>
 
             <ul className="list-none pl-5">
-              <li className="relative ml-5 mb-2 before:content-['💠'] before:left-0 before:text-yellow-500 before:text-lg">
+              <li data-aos="fade-up" data-aos-delay="300" className="relative ml-5 mb-2 before:content-['💠'] before:left-0 before:text-yellow-500 before:text-lg">
                 Structural Repair & strengthening.
               </li>
-              <li className="relative ml-5 mb-2 before:content-['💠'] before:left-0 before:text-yellow-500 before:text-lg">
-              Corrosion study & Cathodic protection.
+              <li data-aos="fade-up" data-aos-delay="600" className="relative ml-5 mb-2 before:content-['💠'] before:left-0 before:text-yellow-500 before:text-lg">
+                Corrosion study & Cathodic protection.
               </li>
-              <li className="relative ml-5 mb-2 before:content-['💠'] before:left-0 before:text-yellow-500 before:text-lg">
+              <li data-aos="fade-up" data-aos-delay="900" className="relative ml-5 mb-2 before:content-['💠'] before:left-0 before:text-yellow-500 before:text-lg">
                 Retrofitting and Jacketing.
               </li>
             </ul>
           </div>
 
-          <div className="flex-1 flex justify-center items-center dark:bg-gray-500 p-5">
+          <div
+            data-aos="fade-up"
+            data-aos-delay="300"
+            className="flex-1 flex justify-center items-center dark:bg-gray-500 p-5"
+          >
             <img
               src={aboutUsImage}
               alt="About us"
--- frontend/src/components/Contact/ContactUs.jsx
+++ frontend/src/components/Contact/ContactUs.jsx
@@ -12,10 +12,10 @@ import {
 const Contact = () => {
   return (
     <>
-      <h1 className="text-center pt-12 text-5xl dark:text-white dark:bg-gray-900">Conatct Us</h1>
+      <h1 data-aos="fade-up" className="text-center pt-12 text-5xl dark:text-white dark:bg-gray-900">Conatct Us</h1>
       <section className="flex justify-center items-center dark:bg-gray-900 bg-white h-[70vh] py-10">
         <div className="border-2 border-gray-500 flex flex-col md:flex-row bg-white rounded-lg shadow-lg max-w-6xl w-full overflow-hidden">
-          <div className="flex-1 p-4 dark:text-white dark:bg-gray-500">
+          <div data-aos="fade-up" data-aos-delay="300" className="flex-1 p-4 dark:text-white dark:bg-gray-500">
             <div className="flex items-center justify-center mt-10">
               <a href="https://www.map.com">
                 <FaMapMarkerAlt
@@ -24,7 +24,7 @@ const Contact = () => {
                 />
               </a>
             </div>
-            <div>
+            <div className="w-[50%] mx-auto text-center mt-2">
               <h2 className="text-center mt-2 text-2xl">Location</h2>
             </div>
             <div className="w-[50%] mx-auto text-center mt-2">
@@ -34,14 +34,14 @@ const Contact = () => {
               </a>
             </div>
             <div className="flex items-center justify-center mt-16">
-              <a href="mailto:shashankstructuralrepair@gamil.com">
+              <a  data-aos="fade-up" data-aos-delay="600" href="mailto:shashankstructuralrepair@gamil.com">
                 <FaEnvelope
                   className="text-2xl
                      hover:text-black hover:animate-ping duration-300"
                 />
               </a>
             </div>
-            <div className="w-[50%] mx-auto text-center mt-2 mb-10">
+            <div  data-aos="fade-up" data-aos-delay="600" className="w-[50%] mx-auto text-center mt-2 mb-10">
               <a href="mailto:shashankstructuralrepair@gamil.com">
               <h2 className="text-center mt-2 text-2xl">Email</h2>
                 <p className="dark:hover:text-gray-200 hover:text-blue-500 mt-2">
